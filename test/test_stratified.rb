355a527a0839cfef9216eba7322767d8b8c908ab
Claudio Bustos <clbustos@gmail.com>
2008-09-12 04:04:26 +0000

    Added test_stratified

---
+++ test/test_stratified.rb
@@ -0,0 +1,20 @@
+require File.dirname(__FILE__)+'/../lib/rubyss'
+require 'rubyss/multiset'
+require 'rubyss/srs'
+require 'test/unit'
+
+class RubySSStratifiedTestCase < Test::Unit::TestCase
+
+	def initialize(*args)
+		super
+	end
+	def test_mean
+		a=[10,20,30,40,50]
+		b=[110,120,130,140]
+		pop=a+b
+		av=a.to_vector(:scale)
+		bv=b.to_vector(:scale)
+		popv=pop.to_vector(:scale)
+		assert_equal(popv.mean,RubySS::StratifiedSample.mean(av,bv))
+	end
+end
