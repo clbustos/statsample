7c236c77bd206694a3c4657a3e200c9e7e57a77f
Claudio Bustos <clbustos@gmail.com>
2008-12-05 16:03:11 +0000

    Complete demo for item analysis

--- demo/item_analysis.rb
+++ demo/item_analysis.rb
@@ -1,18 +1,30 @@
 #!/usr/bin/ruby
 require File.dirname(__FILE__)+'/../lib/rubyss'
 ds=RubySS::CSV.read(File.dirname(__FILE__)+"/sample_test.csv")
+
+fp=File.new(File.dirname(__FILE__)+"/item_analysis/Analisis.html","w")
+
+[['general', (1..23)], ['word',(24..61)], ['excel',(62..82)], ['pp',(83..92)],['internet',(93..101)]].each{|dim,range|
 ds2={}
-(24..61).each {|v|
+
+    fp.puts("<h1>Dimension: #{dim}</h1>")
+    
+    range.each {|v|
+
     ds2["p"+v.to_s]=ds['p'+v.to_s].recode{|c|
-        c.to_f
+        c.nil? ? nil : c.to_f
     }
     ds2["p"+v.to_s].type=:scale
 }
 
 a= RubySS::Reliability::ItemAnalysis.new(ds2.to_dataset)
-File.open("test.html","w") {|fp|
-    fp.puts a.html_summary
+
+fp.puts(a.html_summary)
+a.svggraph_item_characteristic_curve(File.dirname(__FILE__) + "/item_analysis",dim)
+range.each{|v|
+    fp.puts("<p><img src='#{dim}_p#{v}.svg' /></p>\n")
+}
 }
 
-a.svggraph_correct_responses_distribution(File.dirname(__FILE__)+"/images","internet")
-    
+fp.close
+
