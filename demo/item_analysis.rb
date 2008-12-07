9d24b68843209c4e6f8fba1eda60cb5fe51d6561
Claudio Bustos <clbustos@gmail.com>
2008-12-07 05:25:49 +0000

    Create directory for item_analysis on demo

--- demo/item_analysis.rb
+++ demo/item_analysis.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/ruby
 require File.dirname(__FILE__)+'/../lib/rubyss'
+require 'fileutils.rb'
 ds=RubySS::CSV.read(File.dirname(__FILE__)+"/sample_test.csv")
-
+FileUtils::mkdir_p(File.dirname(__FILE__)+"/item_analysis")
 fp=File.new(File.dirname(__FILE__)+"/item_analysis/Analisis.html","w")
 
 [['general', (1..23)], ['word',(24..61)], ['excel',(62..82)], ['pp',(83..92)],['internet',(93..101)]].each{|dim,range|
